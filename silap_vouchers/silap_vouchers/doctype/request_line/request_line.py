539d171de45fb32daabe4343b3c7e7d16f50dbea
franknyarkoh <33531218+franknyarkoh@users.noreply.github.com>
2019-04-11 11:44:14 +0100

    Update request_line.py

--- silap_vouchers/silap_vouchers/doctype/request_line/request_line.py
+++ silap_vouchers/silap_vouchers/doctype/request_line/request_line.py
@@ -5,9 +5,3 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-
-class RequestLine(Document):
-	quantity = int(input("Quantity: "))
-	price = int(input"Price: "))
-	amount = quantity * price
-	total = sum(amount)
